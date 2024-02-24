8112c60b742724a05cabfc77efaebac8783e553e
MOHAMED BAKULY <119895383+MOHAMEDBAKULY@users.noreply.github.com>
2024-02-24 18:16:08 +0300

    changing files naming conventions

---
+++ PYTHON_WITH_DAVE/meaning.py
@@ -0,0 +1,9 @@
+entryAge = 24
+print('')
+
+# if entryAge > 20:
+#     print("Kaa kwa corner")
+# else:
+#     print("Welcome to the table")
+
+print("Build your startUp") if entryAge > 20 else print("Build up your skills first")
---
+++ PYTHON_WITH_DAVE/welcome.py
@@ -0,0 +1,12 @@
+line01 = ("************************") # Header and Footer
+line02 = ("*                      *") # re-use
+line03 = ("*       WELCOME!!      *")
+line04 = ("************************")
+
+# Starts with a blank line then prints
+print('')
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
